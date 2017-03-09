db786abed0c6177a83d713de2a5e225e1b285468
jhsware <sebastian@urbantalk.se>
2017-03-10 00:30:37 +0800

    Add items to top or bottom of list when scrolling

--- MainView.js
+++ MainView.js
@@ -3,24 +3,43 @@ var Observable = require('FuseJS/Observable')
 exports.listRowSpacing = Observable(2)
 exports.listData = Observable()
 
-for (var i = 1; i <= 10; i++) {
-    var section = {
-        title: 'Section ' + i,
-        rows: []
+var start = 1
+var end = 10
+
+function addItems (startAt, endAt, addToTop) {
+    for (var i = startAt; addToTop ? i >= endAt : i <= endAt; addToTop ? i-- : i++) {
+        var section = {
+            title: 'Section ' + i,
+            rows: []
+        }
+        for (var j = 1; j <= 5; j++) {
+            section.rows.push({
+                title: 'Row ' + i + ':' + j,
+                height: Math.round(Math.random() * 30) + 30
+            })
+        }
+        if (addToTop) {
+            exports.listData.insertAt(0, section)
+        } else {
+            exports.listData.add(section)
+        }
     }
-    for (var j = 1; j <= 5; j++) {
-        section.rows.push({
-            title: 'Row ' + i + ':' + j,
-            height: Math.round(Math.random() * 30) + 30
-        })
+    if (addToTop) {
+        if (start === undefined || endAt < start) {
+            start = endAt
+        }
     }
-    exports.listData.add(section)
+
+    if (end === undefined || endAt > end) {
+        end = endAt
+    }
 }
+addItems(start, end)
 
 exports.doLoadAfter = function () {
-
+    addItems(end + 1, end + 3)
 }
 
 exports.doLoadBefore = function () {
-
+    addItems(start - 1, start - 3, true)
 }
